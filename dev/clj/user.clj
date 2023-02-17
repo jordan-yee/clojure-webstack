f99a70ee41e3464b035d5c93aaf2cd35400b6f91
Jordan Yee <jyee117@gmail.com>
2023-02-16 20:50:12 -0700

    Update user ns

--- dev/clj/user.clj
+++ dev/clj/user.clj
@@ -1,9 +1,15 @@
 (ns user
-  #_{:clj-kondo/ignore [:refer-all]}
+  "Initial REPL namespace. Contains development-time helpers & utilities."
+  #_{:clj-kondo/ignore [:unused-namespace :unused-referred-var :refer-all]}
   (:require
-   [dev :refer :all]
+   [clojure.repl :refer :all]
+   [dev :refer [go reset]]
    [portal.api :as portal]))
 
 (defn start-portal []
   (portal/open)
   (portal/tap))
+
+(defn stop-portal []
+  (portal/clear)
+  (portal/close))
