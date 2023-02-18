3ff68a72ce739930bcd49260de365989229032d9
Jordan Yee <jyee117@gmail.com>
2023-02-18 10:21:51 -0700

    Update page-home-test to use ring.mock

--- test/clj/webstack/handlers/page_home_test.clj
+++ test/clj/webstack/handlers/page_home_test.clj
@@ -1,14 +1,20 @@
 (ns webstack.handlers.page-home-test
-  #_{:clj-kondo/ignore [:refer-all]}
   (:require
    [clojure.test :refer :all]
+   [ring.mock.request :as mock]
    [webstack.handlers.page-home :as page-home]))
 
 (deftest handler-test
-  (let [request {}
-        expected-response {:status 200
-                           :headers {"Content-Type" "text/html"}
-                           :body "This is the home page!"}
-        actual-response (page-home/handler request)]
-    (is (= expected-response actual-response)
-        "expected static response is returned")))
+  (let [request (mock/request :get  "/")
+        response (page-home/handler request)]
+    (is (= 200 (:status response))
+        "the home page request returns a success response")
+    (is (and (= "text/html" (get-in response [:headers "Content-Type"]))
+             (string? (:body response)))
+        "the home page request returns an html string")
+    ;; TODO:
+    #_(is (string? (:body response))
+          "the home page request returns valid html"))
+  (is (= (page-home/handler (mock/request :get  "/"))
+         (page-home/handler (mock/request :get  "")))
+      "home page is loaded with or without the trailing slash"))
