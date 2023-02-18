d4ccd8097917432e96dc72a4a6453ec6e062cfb1
Jordan Yee <jyee117@gmail.com>
2023-02-18 16:04:32 -0700

    Update page-home handler to return a hiccup-defined page

--- src/clj/webstack/handlers/page_home.clj
+++ src/clj/webstack/handlers/page_home.clj
@@ -1,15 +1,31 @@
 (ns webstack.handlers.page-home
   "The handler that returns the home page."
   (:require
+   [hiccup.page :as h]
    [ring.util.response :as response]))
 
-(def last-request (atom nil))
-(defn handler [request]
-  (reset! last-request request)
-  (let [body "This is the home page!"]
-    (-> body
-        (response/response)
-        (response/content-type "text/html"))))
+(defn loading-indicator
+  "Returns the Hiccup data to render a loading indicator."
+  []
+  ;; TODO: Center content on page
+  [:div "Loading..."])
+
+(def home-page
+  (h/html5
+   [:head
+    ;; TODO: Register style sheets here:
+    (h/include-css "")]
+   [:body
+    [:div#root
+     [:h1 "Home"] ;; TODO: Remove this once the client-side infrastructure is set up.
+     (loading-indicator)]
+    ;; TODO: Register CDN-based JS libs here:
+    (h/include-js "")]))
+
+(defn handler [_]
+  (-> home-page
+      (response/response)
+      (response/content-type "text/html")))
 
 (def route {:name ::home
             :get handler})
