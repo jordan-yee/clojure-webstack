7edab30af12523785495a243317187244758043b
Jordan Yee <jyee117@gmail.com>
2023-02-18 17:20:49 -0700

    Add content-type-json? test util function

--- test/clj/webstack/test_utils/response.clj
+++ test/clj/webstack/test_utils/response.clj
@@ -1,8 +1,11 @@
 (ns webstack.test-utils.response
   "Test utilities & helpers for page handlers.")
 
-(defn has-content-type? [content-type response]
+(defn- has-content-type? [content-type response]
   (= content-type (get-in response [:headers "Content-Type"])))
 
 (defn content-type-html? [response]
   (has-content-type? "text/html" response))
+
+(defn content-type-json? [response]
+  (has-content-type? "application/json" response))
