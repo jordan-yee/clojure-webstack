c888cd66d08073704000a909cb4cee33c9451e46
Jordan Yee <jyee117@gmail.com>
2023-02-20 15:57:02 -0700

    Configure `ring.middleware.defaults` middleware

--- src/clj/webstack/router.clj
+++ src/clj/webstack/router.clj
@@ -2,15 +2,47 @@
   "Route definitions for the web server."
   (:require
    [reitit.ring :as ring]
+   [ring.middleware.defaults :as middleware-defaults]
    [ring.middleware.json :as middleware-json]
    [webstack.handlers.api-data :as api-data]
    [webstack.handlers.default :as default]
    [webstack.handlers.page-home :as page-home]
    [webstack.middleware.request-observer :as request-observer]))
 
+(defn- get-wrap-defaults
+  "Returns a middleware function for the target `route-type` based on execution
+  environment (dev/prod).
+
+  `route-type` - #{:page :api}
+
+  https://github.com/ring-clojure/ring-defaults"
+  [route-type]
+  (fn [handler]
+    (let [;; TODO: Set this based on env var, or something:
+          environment "dev"]
+      (middleware-defaults/wrap-defaults
+       handler
+       (cond
+         (and (= "dev" environment) (= :page route-type))
+         middleware-defaults/site-defaults
+
+         (and (= "prod" environment) (= :page route-type))
+         middleware-defaults/secure-site-defaults
+
+         (and (= "dev" environment) (= :api route-type))
+         middleware-defaults/api-defaults
+
+         (and (= "prod" environment) (= :api route-type))
+         middleware-defaults/secure-api-defaults
+
+         :else
+         (throw (Exception.
+                 "Invalid or unrecognized execution environment and route-type set!")))))))
+
 (def page-routes
-  [["/" page-home/route]
-   ["/api/v1" {:middleware [[middleware-json/wrap-json-response]]}
+  [["/" page-home/route {:middleware [[(get-wrap-defaults :page)]]}]
+   ["/api/v1" {:middleware [[(get-wrap-defaults :api)
+                             middleware-json/wrap-json-response]]}
     ["/data" api-data/route]]])
 
 (def app (ring/ring-handler
--- test/clj/webstack/router_test.clj
+++ test/clj/webstack/router_test.clj
@@ -4,6 +4,9 @@
    [ring.mock.request :as mock]
    [webstack.router :as router]))
 
+;; TODO: Add tests with requests against `router/app` to test external
+;; middleware as each feature is actually utilized.
+
 (deftest default-route-test
   (let [bad-request (mock/request :get "/page-that-doesnt-exist")
         response (router/app bad-request)]
