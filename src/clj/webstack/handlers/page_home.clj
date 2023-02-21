279da3b1a31cce8ce9934091be6973a5c14f19b3
Jordan Yee <jyee117@gmail.com>
2023-02-20 22:47:12 -0700

    Remove TODO comments

--- src/clj/webstack/handlers/page_home.clj
+++ src/clj/webstack/handlers/page_home.clj
@@ -7,20 +7,17 @@
 (defn loading-indicator
   "Returns the Hiccup data to render a loading indicator."
   []
-  ;; TODO: Center content on page
   [:div.flex-center
    [:h1 "Loading..."]])
 
 (def home-page
   (h/html5
    [:head
-    ;; TODO: Register style sheets here:
     (h/include-css "/css/styles.css")]
    [:body
     [:div.flex-top-level-container
      [:div#root.flex-page-container
       (loading-indicator)]]
-    ;; TODO: Register JavaScript files here:
     (h/include-js "")]))
 
 (defn handler [_]
