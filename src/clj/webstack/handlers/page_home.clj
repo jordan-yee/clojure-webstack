57ea97c3cbc69ba82d389815c1054d8dc85fbe35
Jordan Yee <jyee117@gmail.com>
2023-02-20 22:45:37 -0700

    Configure styles fo centered loading message

--- src/clj/webstack/handlers/page_home.clj
+++ src/clj/webstack/handlers/page_home.clj
@@ -8,18 +8,19 @@
   "Returns the Hiccup data to render a loading indicator."
   []
   ;; TODO: Center content on page
-  [:div "Loading..."])
+  [:div.flex-center
+   [:h1 "Loading..."]])
 
 (def home-page
   (h/html5
    [:head
     ;; TODO: Register style sheets here:
-    (h/include-css "")]
+    (h/include-css "/css/styles.css")]
    [:body
-    [:div#root
-     [:h1 "Home"] ;; TODO: Remove this once the client-side infrastructure is set up.
-     (loading-indicator)]
-    ;; TODO: Register CDN-based JS libs here:
+    [:div.flex-top-level-container
+     [:div#root.flex-page-container
+      (loading-indicator)]]
+    ;; TODO: Register JavaScript files here:
     (h/include-js "")]))
 
 (defn handler [_]
