2d18bee6f1ea47e098e5ed6dd6c141f3bd054f50
Jordan Yee <jyee117@gmail.com>
2023-02-23 23:28:14 -0700

    Configure re-frame w/ re-frame-10x

--- src/cljs/webstack/core.cljs
+++ src/cljs/webstack/core.cljs
@@ -1,12 +1,35 @@
 (ns webstack.core
   "This is the entry point into the ClojureScript client application."
   (:require
-   [reagent.dom :as rd]))
+   [reagent.dom :as rd]
+   [re-frame.core :as rf]))
+
+(rf/reg-event-db
+ ::set-initial-data
+ (fn [db [_ initial-data]]
+   (when-not (empty? db)
+     (js/console.warn "`app-db` is not empty, but is being initialized with new data")
+     (js/console.warn "`app-db` value being overwritten:" db))
+   initial-data))
+
+(rf/reg-sub
+ ::home-page-content
+ (fn [db _]
+   (get-in db [:home-page :content])))
+
+(rf/reg-event-db
+ ::update-welcome-message
+ (fn [db [_ new-message]]
+   (assoc-in db [:home-page :content] new-message)))
 
 (defn home-page []
-  [:div
-   [:h1 "Home Page!"]
-   [:p "This is the home page!"]])
+  (let [content @(rf/subscribe [::home-page-content])]
+    [:div
+     [:h1 "Home Page"]
+     [:button
+      {:on-click #(rf/dispatch [::update-welcome-message "Welcome to the updated Webstack!"])}
+      "Update"]
+     [:p content]]))
 
 (defn page-container [content]
   [:div {:style {:margin "1em"}}
@@ -21,8 +44,10 @@
   [f]
   (js/setTimeout (fn [_] (f)) 1000))
 
-(defn init []
+(defn ^:export main []
   (println "Client started...")
+  (rf/dispatch-sync [::set-initial-data {:home-page
+                                         {:content "Welcome to Webstack!"}}])
   (fake-load mount-app))
 
-(init)
+(main)
