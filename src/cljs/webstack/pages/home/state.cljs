e6adb97d6fdd0f9a20b0a381ce0ae2106abb6bfd
Jordan Yee <jyee117@gmail.com>
2023-02-26 16:18:45 -0700

    Refactor home page state to use get/assoc-in-context functions

--- src/cljs/webstack/pages/home/state.cljs
+++ src/cljs/webstack/pages/home/state.cljs
@@ -7,16 +7,18 @@
 (def initial-values {:content "Welcome to Webstack!"})
 
 (def root-path [:pages :home])
+(def get-in-context (rfh/make-get-in-context root-path))
+(def assoc-in-context (rfh/make-assoc-in-context root-path))
 
 (rf/reg-sub
  ::welcome-message
  (fn [db _]
-   (get-in db [:pages :home :content])))
+   (get-in-context db :content)))
 
 (rfh/reg-event-db
  ::update-welcome-message
  (fn [db [_ new-message]]
-   (assoc-in db [:pages :home :content] new-message)))
+   (assoc-in-context db :content new-message)))
 
 (rf/reg-fx
  ::alert
