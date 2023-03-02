05afc691fdd070604011776df1df25639b704330
Jordan Yee <jyee117@gmail.com>
2023-03-01 23:24:43 -0700

    Update mock other page markup

--- src/cljs/webstack/router/routes.cljs
+++ src/cljs/webstack/router/routes.cljs
@@ -5,7 +5,7 @@
 (def other-route
   "XXX: Remove me I'm only here to enable a routing example."
   {:name ::other
-   :view (fn [] [:div "other"])
+   :view (fn [] [:div>h1 "Other Page"])
    :controllers [{:start (fn [_] (js/console.log :start "other-page start"))
                   :stop (fn [_] (js/console.log :start "other-page stop"))}]})
 
