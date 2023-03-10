e8edea1085f25326fe939f5e4c7ff2d194a60af6
Jordan Yee <jyee117@gmail.com>
2023-03-09 21:26:12 -0700

    Refactor button-bar component in home view

--- src/cljs/webstack/pages/home/view.cljs
+++ src/cljs/webstack/pages/home/view.cljs
@@ -4,17 +4,21 @@
    [webstack.re-frame-helpers :refer [<sub >evt] :as rfh]
    [webstack.pages.home.state :as state]))
 
+(defn button-bar []
+  [:div
+   [:button
+    {:on-click #(>evt [::state/update-welcome-message "Welcome to Webstack!"])}
+    "Yes"]
+   [:button
+    {:on-click #(>evt [::state/update-welcome-message "Welcome back to Webstack!"])}
+    "No"]
+   [:button
+    {:on-click #(>evt [::state/update-welcome-message (:content state/initial-values)])}
+    "Reset"]])
+
 (defn home-page []
   (let [content (<sub [::state/welcome-message])]
     [:div
      [:h1 "Home Page"]
      [:p content]
-     [:button
-      {:on-click #(>evt [::state/update-welcome-message "Welcome to Webstack!"])}
-      "Yes"]
-     [:button
-      {:on-click #(>evt [::state/update-welcome-message "Welcome back to Webstack!"])}
-      "No"]
-     [:button
-      {:on-click #(>evt [::state/update-welcome-message (:content state/initial-values)])}
-      "Reset"]]))
+     [button-bar]]))
