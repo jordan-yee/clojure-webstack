5fa9a3ba17d15843dd4e9d1aa92758815e14e161
Jordan Yee <jyee117@gmail.com>
2023-03-09 21:14:01 -0700

    Var-quote route view function (reagent component) to fix hot reloading

--- src/cljs/webstack/pages/home/route.cljs
+++ src/cljs/webstack/pages/home/route.cljs
@@ -3,6 +3,6 @@
    [webstack.pages.home.view :refer [home-page]]))
 
 (def route {:name ::home
-            :view home-page
+            :view #'home-page
             :controllers [{:start (fn [_] (js/console.log :start "home-page start"))
                            :stop (fn [_] (js/console.log :start "home-page stop"))}]})
